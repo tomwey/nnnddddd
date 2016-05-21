985f208fea45eb0cde5ada444ba781a1910a273f
Tomwey <tomwey@163.com>
2016-05-21 22:34:51 +0800

    s

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,12 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def super_admin?
+    Setting.admin_emails.include?(self.email)
+  end
+  
+  def admin?
+    SiteConfig.admin_emails.include?(self.email) or super_admin?
+  end
 end
