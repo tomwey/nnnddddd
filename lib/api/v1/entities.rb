c4817e1871ad80e1c0b8a1477eac706e8658c2e0
Tomwey <tomwey@163.com>
2016-06-11 19:34:09 +0800

    dddd

--- lib/api/v1/entities.rb
+++ lib/api/v1/entities.rb
@@ -96,7 +96,7 @@ module API
         expose :content, format_with: :null
         expose :stream_id, format_with: :null
         # expose :author_name, as: :author
-        expose :author, using: API::V1::Entities::Author, if: Proc.new { |author| author.present? }
+        expose :author, using: API::V1::Entities::Author, if: Proc.new { |b| b.author_id.present? }
         expose :location, format_with: :null
         expose :created_at, as: :sent_at, format_with: :chinese_datetime
       end
