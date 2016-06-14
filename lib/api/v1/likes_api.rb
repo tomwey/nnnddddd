bd84337894ef03a4d0c529ddd90f0e90d4b28fc8
Tomwey <tomwey@163.com>
2016-06-14 19:30:26 +0800

    update like func

--- lib/api/v1/likes_api.rb
+++ lib/api/v1/likes_api.rb
@@ -22,11 +22,11 @@ module API
       
       # 用户收藏视频相关
       resource :user, desc: '用户相关接口' do 
-        desc "用户收藏"
+        desc "用户点赞"
         params do
           requires :token,       type: String,  desc: "用户认证Token"
-          requires :likeable_id, type: Integer, desc: "被收藏对象的ID，比如视频的ID"
-          optional :type,        type: Integer, desc: '视频流类型，推荐里面的视频type值为2，直播为1，如果不传该参数，默认为收藏推荐里面的视频'
+          requires :likeable_id, type: Integer, desc: "被点赞对象的ID，比如视频的ID"
+          optional :type,        type: Integer, desc: '视频流类型，推荐里面的视频type值为2，直播为1，如果不传该参数，默认为点赞推荐里面的视频'
         end
         post :like do
           user = authenticate!
@@ -40,20 +40,20 @@ module API
           klass = likeable_type.classify.constantize
           
           likeable = klass.find_by(id: params[:likeable_id])
-          return render_error(3001, '您已经收藏过了') if user.liked?(likeable)
+          return render_error(3001, '您已经点赞过了') if user.liked?(likeable)
           
           if user.like!(likeable)
             render_json_no_data
           else
-            render_error(3002, 'Oops, 收藏失败了!')
+            render_error(3002, 'Oops, 点赞失败了!')
           end
         end # end post like
         
-        desc "取消收藏"
+        desc "取消点赞"
         params do
           requires :token, type: String, desc: "用户认证Token"
-          requires :likeable_id, type: Integer, desc: "被收藏对象的ID，比如视频的ID"
-          optional :type,        type: Integer, desc: '视频流类型，推荐里面的视频type值为2，直播为1，如果不传该参数，默认为收藏推荐里面的视频'
+          requires :likeable_id, type: Integer, desc: "被点赞对象的ID，比如视频的ID"
+          optional :type,        type: Integer, desc: '视频流类型，推荐里面的视频type值为2，直播为1，如果不传该参数，默认为点赞推荐里面的视频'
         end
         post :cancel_like do
           user = authenticate!
@@ -68,16 +68,16 @@ module API
           klass = likeable_type.classify.constantize
           
           likeable = klass.find_by(id: params[:likeable_id])
-          return render_error(3001, '您还未收藏,不能取消') unless user.liked?(likeable)
+          return render_error(3001, '您还未点赞,不能取消点赞') unless user.liked?(likeable)
           
           if user.cancel_like!(likeable)
             render_json_no_data
           else
-            render_error(3002, 'Oops, 取消收藏失败了!')
+            render_error(3002, 'Oops, 取消点赞失败了!')
           end
         end # end post like
         
-        desc "获取用户收藏过的视频"
+        desc "获取用户赞过的视频"
         params do
           requires :token, type: String, desc: "用户认证Token"
           use :pagination
