29aee4fa573ee2725e97d81041b35ad83372e587
Tomwey <tomwey@163.com>
2016-07-04 23:50:43 +0800

    add banner api

--- lib/api/v1/banners_api.rb
+++ lib/api/v1/banners_api.rb
@@ -12,6 +12,16 @@ module API
           @banners = Banner.where(category_id: nil).sorted.recent.limit(size)
           render_json(@banners, API::V1::Entities::Banner)
         end
+        
+        desc '获取某个视频类别下面的广告'
+        # params do
+        #   requires :cid, type: Integer, desc: '类别ID'
+        # end
+        get '/category/:category_id' do
+          @banners = Banner.where(category_id: params[:category_id]).sorted.recent
+          render_json(@banners, API::V1::Entities::Banner)
+        end
+        
       end # end resource
       
     end
